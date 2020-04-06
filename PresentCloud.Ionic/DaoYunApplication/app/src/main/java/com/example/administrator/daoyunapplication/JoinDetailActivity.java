3b0fe11da75d4579814d40438e1fb4b7b3fef46a
hongqiyu <1069619201@qq.com>
2020-04-06 11:34:32 +0800

    增加加入班课的第二个页面

---
+++ PresentCloud.Ionic/DaoYunApplication/app/src/main/java/com/example/administrator/daoyunapplication/JoinDetailActivity.java
@@ -0,0 +1,11 @@
+package com.example.administrator.daoyunapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class JoinDetailActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_detail);
+    }
+}
