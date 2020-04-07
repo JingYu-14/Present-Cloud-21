885c3dca131cbd60918b1c9633b6738ae1a9a419
hongqiyu <1069619201@qq.com>
2020-04-07 12:01:34 +0800

    班课详情页面

---
+++ PresentCloud.Ionic/DaoYunApplication/app/src/main/java/com/example/administrator/daoyunapplication/MemberActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.daoyunapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.daoyunapplication.R;
+
+
+public class MemberActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member);
+    }
+}
