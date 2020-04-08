2005b085fced45688c9b7c93c2aedc7dc9cc96f8
hongqiyu <1069619201@qq.com>
2020-04-08 09:30:14 +0800

    资源、成员、活动、详情页面的跳转

--- PresentCloud.Ionic/DaoYunApplication/app/src/main/java/com/example/administrator/daoyunapplication/ClassActivity.java
+++ PresentCloud.Ionic/DaoYunApplication/app/src/main/java/com/example/administrator/daoyunapplication/ClassActivity.java
@@ -1,12 +1,39 @@
 package com.example.administrator.daoyunapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ClassActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class);
+        ImageView imageView=(ImageView)findViewById(R.id.cy_zy);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ClassActivity.this,MemberActivity.class);
+                startActivity(intent);
+            }
+        });
+        ImageView hdImage=(ImageView)findViewById(R.id.hd_zy);
+        hdImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent hdintent=new Intent(ClassActivity.this,ActivityActivity.class);
+                startActivity(hdintent);
+            }
+        });
+        ImageView detailImage=(ImageView)findViewById(R.id.xq_zy);
+        detailImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detailIntent=new Intent(ClassActivity.this,DetailActivity.class);
+                startActivity(detailIntent);
+            }
+        });
     }
 }
