d77952dd2fb2d44056ee0dffd9811b8ad78af3a0
hongqiyu <1069619201@qq.com>
2020-04-08 09:30:14 +0800

    资源、成员、活动、详情页面的跳转

--- PresentCloud.Ionic/DaoYunApplication/app/src/main/java/com/example/administrator/daoyunapplication/ActivityActivity.java
+++ PresentCloud.Ionic/DaoYunApplication/app/src/main/java/com/example/administrator/daoyunapplication/ActivityActivity.java
@@ -1,7 +1,10 @@
 package com.example.administrator.daoyunapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class ActivityActivity extends AppCompatActivity {
@@ -9,5 +12,29 @@ public class ActivityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity);
+        ImageView imageView=(ImageView)findViewById(R.id.cy_hd);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ActivityActivity.this,MemberActivity.class);
+                startActivity(intent);
+            }
+        });
+        ImageView hdImage=(ImageView)findViewById(R.id.zy_hd);
+        hdImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent hdintent=new Intent(ActivityActivity.this,ClassActivity.class);
+                startActivity(hdintent);
+            }
+        });
+        ImageView detailImage=(ImageView)findViewById(R.id.xq_hd);
+        detailImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detailIntent=new Intent(ActivityActivity.this,DetailActivity.class);
+                startActivity(detailIntent);
+            }
+        });
     }
 }
--- PresentCloud.Ionic/DaoYunApplication/app/src/main/java/com/example/administrator/daoyunapplication/DetailActivity.java
+++ PresentCloud.Ionic/DaoYunApplication/app/src/main/java/com/example/administrator/daoyunapplication/DetailActivity.java
@@ -1,12 +1,39 @@
 package com.example.administrator.daoyunapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        ImageView imageView=(ImageView)findViewById(R.id.cy_xq);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DetailActivity.this,MemberActivity.class);
+                startActivity(intent);
+            }
+        });
+        ImageView hdImage=(ImageView)findViewById(R.id.hd_xq);
+        hdImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent hdintent=new Intent(DetailActivity.this,ActivityActivity.class);
+                startActivity(hdintent);
+            }
+        });
+        ImageView detailImage=(ImageView)findViewById(R.id.zy_xq);
+        detailImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detailIntent=new Intent(DetailActivity.this,ClassActivity.class);
+                startActivity(detailIntent);
+            }
+        });
     }
 }
