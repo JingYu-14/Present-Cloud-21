650b0d35971af41d9c1ce47a5794760fc234810c
hongqiyu <1069619201@qq.com>
2020-04-10 10:27:34 +0800

    教师查看学员信息页面（还未增加排序）

---
+++ PresentCloud.Ionic/DaoYunApplication/app/src/main/java/com/example/administrator/daoyunapplication/MemberTeacherActivity.java
@@ -0,0 +1,42 @@
+package com.example.administrator.daoyunapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.administrator.daoyunapplication.R;
+
+
+public class MemberTeacherActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_teacher);
+//        ImageView imageView=(ImageView)findViewById(R.id.zy_cy);
+//        imageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent=new Intent(MemberTeacherActivity.this,ClassActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//        ImageView hdImage=(ImageView)findViewById(R.id.hd_cy);
+//        hdImage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent hdintent=new Intent(MemberActivity.this,ActivityActivity.class);
+//                startActivity(hdintent);
+//            }
+//        });
+//        ImageView detailImage=(ImageView)findViewById(R.id.xq_cy);
+//        detailImage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent detailIntent=new Intent(MemberActivity.this,DetailActivity.class);
+//                startActivity(detailIntent);
+//            }
+//        });
+    }
+}
