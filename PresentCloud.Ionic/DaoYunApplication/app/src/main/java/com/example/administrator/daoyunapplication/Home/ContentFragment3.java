3a6ee88101575b23ca180ddca7ca1e6aff0bc323
zhangyin <1143047418@qq.com>
2020-04-24 20:00:10 +0800

    修改用户信息功能

--- PresentCloud.Ionic/DaoYunApplication/app/src/main/java/com/example/administrator/daoyunapplication/Home/ContentFragment3.java
+++ PresentCloud.Ionic/DaoYunApplication/app/src/main/java/com/example/administrator/daoyunapplication/Home/ContentFragment3.java
@@ -169,28 +169,28 @@ public class ContentFragment3 extends ListFragment//extends Fragment
     EditText update_email_input;
     EditText update_telephone_input;
     public void setUser(View view){
-        update_account_input=(EditText) view.findViewById(R.id.update_account_input);
-        update_nick_input=(EditText)view.findViewById(R.id.update_nick_input);
+//        update_account_input=(EditText) view.findViewById(R.id.update_account_input);
+//        update_nick_input=(EditText)view.findViewById(R.id.update_nick_input);
         update_password_input=(EditText)view.findViewById(R.id.update_password_input);
         update_name_input=(EditText)view.findViewById(R.id.update_name_input);
-        update_email_input=(EditText)view.findViewById(R.id.update_email_input);
-        update_telephone_input=(EditText)view.findViewById(R.id.update_telephone_input);
-        update_account_input.setText(user.getUsername());
-        update_nick_input.setText(user.getNick());//user是null
+//        update_email_input=(EditText)view.findViewById(R.id.update_email_input);
+//        update_telephone_input=(EditText)view.findViewById(R.id.update_telephone_input);
+//        update_account_input.setText(user.getUsername());
+//        update_nick_input.setText(user.getNick());//user是null
         update_password_input.setText(user.getPassword());
         update_name_input.setText(user.getName());
-        update_email_input.setText(user.getEmail());
-        update_telephone_input.setText(user.getTelephone());
+//        update_email_input.setText(user.getEmail());
+//        update_telephone_input.setText(user.getTelephone());
     }
     String password;
     String name;
     public void updateUser(){
-        update_account_input.getText().toString();
-        update_nick_input.getText().toString();
+//        update_account_input.getText().toString();
+//        update_nick_input.getText().toString();
         password= update_password_input.getText().toString();
         name = update_name_input .getText().toString();
-        update_email_input.getText().toString();
-        update_telephone_input.getText().toString();
+//        update_email_input.getText().toString();
+//        update_telephone_input.getText().toString();
         yesUpdataUser(name,password);
 
 
